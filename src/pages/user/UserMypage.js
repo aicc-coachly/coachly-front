f8b3d0ffb6362e8db7d4f6faf675b4213149bb62
80ps <yjsion8085@gmail.com>
2024-11-01 15:16:08 +0900

    Feat : test

--- src/pages/user/UserMypage.js
+++ src/pages/user/UserMypage.js
@@ -1,18 +1,13 @@
-import React from 'react'
-import { useModal } from '../../components/common/ModalProvider';
-import { BodyCompositionModal } from '../../components/user/BodyCompositionModal';
-import { CheckScheduleModal } from '../../components/trainer/CheckScheduleModal';
+import React from 'react';
 
-
-const UserMypage = () => {
-    const { openModal } = useModal();
-
-    return (
-        <div>UserMypage
-        <button onClick={() => openModal(<BodyCompositionModal/>)}>체성분 추가하기</button>
-        <button onClick={() => openModal(<CheckScheduleModal/>)}>일정 자세히보기</button>
+function UserMypage() {
+  return (
+    <div className="max-w-md mx-auto p-4">
+      <h2 className="text-2xl font-bold mb-4">회원 메인 페이지</h2>
+      <p>여기는 회원 전용 메인 페이지입니다.</p>
+      {/* 회원 전용 기능 추가 */}
     </div>
-  )
+  );
 }
 
-export default UserMypage
+export default UserMypage;
