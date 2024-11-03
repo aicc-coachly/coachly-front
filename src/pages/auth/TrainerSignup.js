421235a9da788c39fdfbc7ddd728b0e77e1ede55
80ps <yjsion8085@gmail.com>
2024-11-03 17:36:59 +0900

    Fix: add sevice_option

--- src/pages/auth/TrainerSignup.js
+++ src/pages/auth/TrainerSignup.js
@@ -11,30 +11,30 @@ function TrainerSignup() {
     name: '',
     phone: '',
     gender: '',
-<<<<<<< HEAD
     resume: '',
     trainer_zipcode: '',
     trainer_address: '',
     trainer_detail_address: '',
-    price_options: [{ option: '', amount: '', frequency: '' }], // 가격 옵션을 배열로 관리
+    price_options: { 
+      one_day: { amount: '', isChecked: false }, // 원데이 클래스
+      per_session: { frequency: '', amount: '', isChecked: false }, // 회당 가격
+      inquiry_needed: false // 문의 필요 여부
+    },
     account: '',
     bank_name: '',
     account_name: '',
     main_account: false, // 대표계좌 설정
     service_options: [], // 특기 옵션
-=======
-    specialization: [], // 초기값을 배열로 변경
-    introduction: '',
-    classLocation1: '',
-    classLocation2: '',
-    accountInfo: '',
-    mainAccount: false,
-    classFee: '',
-    consultationFee: '',
-    agreeTerms: false,
->>>>>>> origin/feature/pages/TrainerSignup
+    agreeTerms: false, // 개인정보 수집 동의
   });
+  
   const [selectedImage, setSelectedImage] = useState(null); // 이미지 선택 상태
+  const optionMap = {
+    '여성전문': 1,
+    '선수/대회 전문': 2,
+    '재활전문': 3,
+    '실버 전문': 4,
+  };
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -44,15 +44,38 @@ function TrainerSignup() {
     });
   };
 
-  // 특기 옵션 선택 처리
-  const handleServiceOptionsChange = (option) => {
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      service_options: prevFormData.service_options.includes(option)
-        ? prevFormData.service_options.filter((item) => item !== option)
-        : [...prevFormData.service_options, option],
-    }));
+   // 특기 옵션 선택 핸들러
+   const handleServiceOptionsChange = (option) => {
+    const optionValue = optionMap[option];
+
+    setFormData((prevFormData) => {
+      const currentOptions = prevFormData.service_options;
+
+      // 이미 선택된 옵션이 있는 경우, 선택 해제 처리
+      if (currentOptions.includes(optionValue)) {
+        return {
+          ...prevFormData,
+          service_options: currentOptions.filter((item) => item !== optionValue),
+        };
+      } else {
+        // 선택된 옵션이 2개 미만인 경우에만 추가
+        if (currentOptions.length < 2) {
+          return {
+            ...prevFormData,
+            service_options: [...currentOptions, optionValue],
+          };
+        } else {
+          // 이미 2개가 선택된 경우, 가장 오래된 옵션을 제거하고 새로운 옵션을 추가
+          const updatedOptions = [...currentOptions.slice(1), optionValue];
+          return {
+            ...prevFormData,
+            service_options: updatedOptions,
+          };
+        }
+      }
+    });
   };
+
 
   // 이미지 파일 선택 처리
   const handleImageChange = (e) => {
@@ -79,47 +102,97 @@ function TrainerSignup() {
     }));
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!formData.agreeTerms) { // 체크박스가 체크되지 않은 경우
-      alert('개인정보 수집 및 활용에 동의해야 합니다.');
-      return;
-    }
+// 가격 입력 시 콤마 추가
+const formatAmount = (value) => {
+  return value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
 
-    const data = new FormData();
-    Object.entries(formData).forEach(([key, value]) => {
-      if (Array.isArray(value)) {
-        value.forEach((item) => data.append(`${key}[]`, JSON.stringify(item)));
-      } else {
-        data.append(key, value);
-      }
+const handleAmountChange = (type, value) => {
+  const numericValue = value.replace(/,/g, ''); // 콤마 제거
+  setFormData((prevFormData) => ({
+    ...prevFormData,
+    price_options: {
+      ...prevFormData.price_options,
+      [type]: {
+        ...prevFormData.price_options[type],
+        amount: numericValue, // 숫자 값으로 저장
+      },
+    },
+  }));
+};
+
+const handleFrequencyChange = (value) => {
+  const numericValue = Math.max(0, Math.min(30, value)); // 0~30 범위로 제한
+  setFormData((prevFormData) => ({
+    ...prevFormData,
+    price_options: {
+      ...prevFormData.price_options,
+      per_session: {
+        ...prevFormData.price_options.per_session,
+        frequency: numericValue, // 회당 가격 저장
+      },
+    },
+  }));
+};
+
+const handleCheckboxChange = (type) => {
+  setFormData((prevFormData) => ({
+    ...prevFormData,
+    price_options: {
+      ...prevFormData.price_options,
+      [type]: {
+        ...prevFormData.price_options[type],
+        isChecked: !prevFormData.price_options[type].isChecked, // 체크 상태 토글
+      },
+    },
+  }));
+};
+
+const handleInquiryChange = () => {
+  setFormData((prevFormData) => ({
+    ...prevFormData,
+    price_options: {
+      ...prevFormData.price_options,
+      inquiry_needed: !prevFormData.price_options.inquiry_needed, // 체크 상태 토글
+    },
+  }));
+};
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  if (!formData.agreeTerms) { // 체크박스가 체크되지 않은 경우
+    alert('개인정보 수집 및 활용에 동의해야 합니다.');
+    return;
+  }
+
+  const data = new FormData();
+  Object.entries(formData).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      value.forEach((item) => data.append(`${key}[]`, JSON.stringify(item)));
+    } else {
+      data.append(key, value);
+    }
+  });
+  if (selectedImage) {
+    data.append('image', selectedImage);
+  }
+
+  try {
+    const response = await api.post('/trainerSignup', data, {
+      headers: { 'Content-Type': 'multipart/form-data' },
     });
-    if (selectedImage) {
-      data.append('image', selectedImage);
+    if (response.status === 201) {
+      alert('회원가입이 완료되었습니다!');
+      navigate('/login');
+    } else {
+      alert('회원가입에 실패했습니다.');
     }
- 
-    try {
-<<<<<<< HEAD
-      const response = await api.post('/trainerSignup', data, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
-=======
-      // API 호출 - 백엔드 엔드포인트가 준비되면 URL을 업데이트하세요.
-      const response = await api.post('/trainerSignup', formData);
+  } catch (error) {
+    console.error('회원가입 오류:', error);
+    alert('오류가 발생했습니다. 다시 시도해주세요.');
+  }
+};
 
-      // 서버 응답에 따라 처리
->>>>>>> origin/feature/pages/TrainerSignup
-      if (response.status === 201) {
-        alert('회원가입이 완료되었습니다!');
-        navigate('/login');
-      } else {
-        alert('회원가입에 실패했습니다.');
-      }
-    } catch (error) {
-      console.error('회원가입 오류:', error);
-      alert('오류가 발생했습니다. 다시 시도해주세요.');
-    }
-  };
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -208,71 +281,47 @@ function TrainerSignup() {
             </div>
           </div>
 
-<<<<<<< HEAD
           {/* 특기 옵션 */}
           <div className="mb-4">
             <label className="block mb-2">특기 선택</label>
             <div className="grid grid-cols-2 gap-2">
-              {['여성전문', '선수/대회 전문', '재활전문', '실버 전문'].map((option) => (
+              {Object.keys(optionMap).map((option) => (
                 <div key={option} className="flex flex-col items-center">
                   <label
                     className={`border rounded-lg w-full h-12 flex items-center justify-center cursor-pointer ${
-                      formData.service_options.includes(option) ? 'bg-[#d0e3ff] text-white' : 'border-gray-300'
-=======
-       {/* 특기 선택 */}
-       <div className="mb-4">
-            <label className="block mb-2">특기 선택</label>
-            <div className="grid grid-cols-2 gap-2">
-              {['여성전문', '선수/대회 전문', '남성전문', '실버 전문'].map((specialization) => (
-                <div key={specialization} className="flex flex-col items-center">
-                  <label
-                    className={`border rounded-lg w-full h-12 flex items-center justify-center cursor-pointer ${
-                      formData.specialization.includes(specialization) ? 'bg-[#d0e3ff] text-white' : 'border-gray-300'
->>>>>>> origin/feature/pages/TrainerSignup
+                      formData.service_options.includes(optionMap[option]) ? 'bg-[#d0e3ff] text-white' : 'border-gray-300'
                     }`}
                   >
                     <input
                       type="checkbox"
-<<<<<<< HEAD
                       name="service_options"
                       value={option}
-                      checked={formData.service_options.includes(option)}
+                      checked={formData.service_options.includes(optionMap[option])}
                       onChange={() => handleServiceOptionsChange(option)}
                       className="hidden"
                     />
                     {option}
-=======
-                      name="specialization"
-                      value={specialization}
-                      checked={formData.specialization.includes(specialization)}
-                      onChange={(e) => {
-                        const { value, checked } = e.target;
-                        if (checked) {
-                          // 최대 2개까지 선택 가능
-                          if (formData.specialization.length < 2) {
-                            setFormData({
-                              ...formData,
-                              specialization: [...formData.specialization, value],
-                            });
-                          }
-                        } else {
-                          setFormData({
-                            ...formData,
-                            specialization: formData.specialization.filter((item) => item !== value),
-                          });
-                        }
-                      }}
-                      className="hidden" // 체크박스 숨김
-                    />
-                    {specialization}
->>>>>>> origin/feature/pages/TrainerSignup
                   </label>
                 </div>
               ))}
             </div>
+
+          </div>
+           {/* 자기소개서 */}
+           <div className="mb-4">
+            <label htmlFor="resume" className="block mb-2">자기소개서</label>
+            <textarea
+              id="resume"
+              name="resume"
+              value={formData.resume}
+              onChange={handleChange}
+              className="w-full px-3 py-2 border rounded"
+              placeholder="자기소개를 입력하세요."
+            ></textarea>
           </div>
 
-<<<<<<< HEAD
+
+
           {/* 수업 장소 */}
           <div className="mb-4">
             <label className="block mb-2">수업 장소</label>
@@ -302,49 +351,74 @@ function TrainerSignup() {
             />
           </div>
 
-{/* 가격 옵션 입력 */}
-<div className="mb-4">
-  <label className="block mb-2">가격 옵션</label>
-  
-  {/* 원데이 클래스 */}
-  <div className="flex items-center mb-4">
-    <span className="w-1/4 text-gray-700">원데이 클래스:</span>
-    <input
-      type="text"
-      name="option"
-      value={formData.price_options[0]?.option || ''}
-      onChange={(e) => handlePriceOptionChange(0, 'option', e.target.value)}
-      className="w-1/2 px-3 py-2 border rounded mr-2"
-      placeholder="금액"
-    />
-    <span className="text-gray-700">원</span>
-  </div>
-  
-  {/* 회당 가격 */}
-  <div className="flex items-center mb-4">
-    <span className="w-1/4 text-gray-700">회당 가격:</span>
-    <input
-      type="number"
-      name="frequency"
-      value={formData.price_options[0]?.frequency || ''}
-      onChange={(e) => handlePriceOptionChange(0, 'frequency', e.target.value)}
-      className="w-1/5 px-3 py-2 border rounded mr-2"
-      placeholder="횟수"
-      min="0"
-      max="30"
-    />
-    <span className="text-gray-700">회</span>
-    <input
-      type="text" // 숫자 입력을 위한 텍스트 필드로 변경
-      name="amount"
-      value={formData.price_options[0]?.amount ? formData.price_options[0].amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : ''}
-      onChange={(e) => handlePriceOptionChange(0, 'amount', e.target.value.replace(/,/g, ''))} // 콤마 제거 후 저장
-      className="w-1/2 px-3 py-2 border rounded ml-2"
-      placeholder="금액"
-    />
-    <span className="text-gray-700">원</span>
-  </div>
-</div>
+          {/* 가격 옵션 입력 */}
+          <div className="mb-4">
+            <label className="block mb-2">가격 옵션</label>
+{/* 원데이 클래스 */}
+<div className="flex items-center mb-4">
+              <input
+                type="checkbox"
+                checked={formData.price_options.one_day.isChecked}
+                onChange={() => handleCheckboxChange('one_day')}
+                className="mr-2"
+              />
+              <span className="w-1/4 text-gray-700">원데이 클래스:</span>
+              <input
+                type="text"
+                name="one_day_option"
+                value={formData.price_options.one_day.amount ? formatAmount(formData.price_options.one_day.amount) : ''}
+                onChange={(e) => handleAmountChange('one_day', e.target.value)} // 콤마 추가 후 저장
+                className="w-1/2 px-3 py-2 border rounded mr-2"
+                placeholder="금액"
+              />
+              <span className="text-gray-700">원</span>
+            </div>
+            
+            {/* 회당 가격 */}
+            <div className="flex items-center mb-4">
+              <input
+                type="checkbox"
+                checked={formData.price_options.per_session.isChecked}
+                onChange={() => handleCheckboxChange('per_session')}
+                className="mr-2"
+              />
+              <span className="w-1/4 text-gray-700">회당 가격:</span>
+              <input
+                type="number"
+                name="frequency"
+                value={formData.price_options.per_session.frequency || ''}
+                onChange={(e) => handleFrequencyChange(e.target.value)} // 0~30 범위로 제한
+                className="w-1/5 px-3 py-2 border rounded mr-2"
+                placeholder="횟수"
+                min="0"
+                max="30"
+              />
+              <span className="text-gray-700">회</span>
+              <input
+                type="text"
+                name="amount"
+                value={formData.price_options.per_session.amount ? formatAmount(formData.price_options.per_session.amount) : ''}
+                onChange={(e) => handleAmountChange('per_session', e.target.value)} // 콤마 추가 후 저장
+                className="w-1/2 px-3 py-2 border rounded ml-2"
+                placeholder="금액"
+              />
+              <span className="text-gray-700">원</span>
+            </div>
+          </div>
+
+          {/* 문의 필요 여부 */}
+          <div className="mb-4 flex items-center">
+            <input
+              type="checkbox"
+              id="inquiry_needed"
+              checked={formData.price_options.inquiry_needed}
+              onChange={handleInquiryChange}
+              className="mr-2"
+            />
+            <label htmlFor="inquiry_needed" className="text-sm">
+              문의 필요
+            </label>
+          </div>
 
           <div className="mb-4">
   <label htmlFor="bank_name" className="block mb-2">은행 이름</label>
@@ -405,143 +479,6 @@ function TrainerSignup() {
           </div>
            {/* 개인정보 수집 동의 */}
            <div className="mb-6 flex items-center">
-=======
-        {/* 수업 장소 */}
-        <div className="mb-4">
-          <label className="block mb-2">수업 장소</label>
-                    
-          {/* 우편번호 입력 */}
-          <input
-            type="text"
-            name="postalCode"
-            value={formData.postalCode}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border rounded mb-2"
-            placeholder="우편번호"
-          />
-          
-          {/* 주소 입력 */}
-          <input
-            type="text"
-            name="classLocation"
-            value={formData.classLocation1}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border rounded mb-2"
-            placeholder="주소"
-          />
-          
-          {/* 상세주소 입력 */}
-          <input
-            type="text"
-            name="classLocation2"
-            value={formData.classLocation2}
-            onChange={handleChange}
-            className="w-full px-3 py-2 border rounded"
-            placeholder="상세주소"
-          />
-        </div>
-                    
-                 {/* 계좌 정보 */}
-        <div className="mb-4">
-            <label htmlFor="bankName" className="block mb-2">은행 이름</label>
-              <input
-                type="text"
-                id="bankName"
-                name="bankName"
-                value={formData.bankName}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border rounded mb-2"
-                placeholder="은행 이름을 입력하세요"
-              />
-          
-            <label htmlFor="accountInfo" className="block mb-2">계좌 정보</label>
-              <input
-                type="text"
-                id="accountInfo"
-                name="accountInfo"
-                value={formData.accountInfo}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border rounded"
-                placeholder="계좌 정보를 입력하세요"
-              />
-          <div className="mt-2">
-            <label>
-              <input
-                type="checkbox"
-                name="mainAccount"
-                checked={formData.mainAccount}
-                onChange={handleChange}
-                className="mr-2"
-              />
-              대표 계좌로 설정
-            </label>
-          </div>
-        </div>
-
-
-{/* 가격 정보 */}
-<div className="mb-4">
-    <label className="block bold-2 mb-2">가격</label>
-    <div className="flex items-center mb-2">
-        <label className="block mr-2 w-1/2" htmlFor="oneDayClassFee">원데이클래스</label>
-        <input
-            type="text"
-            id="oneDayClassFee"
-            name="oneDayClassFee"
-            value={formData.classFee.replace(/\B(?=(\d{3})+(?!\d))/g, ",")} // 콤마 추가
-            onChange={(e) => {
-                const value = e.target.value.replace(/,/g, '');
-                setFormData({ ...formData, classFee: value });
-            }}
-            className="w-5/6 px-3 py-2 border rounded"
-            placeholder="원데이 클래스 비용 입력"
-        />
-    </div>
-
-
-</div>
-
-{/* 회당 가격 정보 */}
-<div className="mb-4">
-<div className="flex items-center mb-2 justify-end">
-    <label className="block w-1/3 mb-2">회당 가격</label>
-    <div className="flex items-center mb-2">
-        {/* 회수 선택 */}
-        <input
-            type="number"
-            name="consultationCount"
-            value={formData.consultationCount || 0} // 초기값 0으로 설정
-            onChange={(e) => {
-                const value = Math.max(0, Math.min(30, e.target.value)); // 0에서 30 사이로 제한
-                setFormData({ ...formData, consultationCount: value });
-            }}
-            className="w-13 px-3 py-2 border rounded mx-2"
-            min="0"
-            max="30"
-            style={{ appearance: 'auto' }}
-        />
-        
-        {/* 가격 입력 */}
-        <input
-            type="text"
-            name="consultationFee"
-            value={formData.consultationFee.replace(/\B(?=(\d{3})+(?!\d))/g, ",")} // 콤마 추가
-            onChange={(e) => {
-                const value = e.target.value.replace(/,/g, ''); // 콤마 제거
-                setFormData({ ...formData, consultationFee: value });
-            }}
-            className="w-5/6 px-3 py-2 border rounded"
-            placeholder="가격 입력"
-        />
-    </div>
-</div>
-</div>
-          
-
-
-          {/* 개인정보 수집 동의 */}
-          <div className="mb-6 flex items-center">
->>>>>>> origin/feature/pages/TrainerSignup
             <input
               type="checkbox"
               id="agreeTerms"
