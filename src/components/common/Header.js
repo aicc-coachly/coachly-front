cfe5179e0c1cd8c98267fa9d8f3f08271a70bdba
HHJ74 <gpgus0704@gmail.com>
2024-11-05 11:15:42 +0900

    Feat :  change Page navigation and navbar button types change based on user type

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,32 +1,20 @@
 // src/components/common/Header.js
 import React, { useState } from 'react';
-import { useLocation, Link } from 'react-router-dom';
+import {Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { UserMenuButtons, TrainerMenuButtons } from './Buttons';
 import logo from '../../assets/images/logo.png';
 
 function Header() {
-  const location = useLocation();
   const [menuOpen, setMenuOpen] = useState(false);
+  const { userType, isLoggedIn } = useSelector((state) => state.auth); // Redux 상태 가져오기
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  // 특정 페이지에서 메뉴바 숨김
-  const hideMenuBar = [
-    '/',
-    '/sortsignup',
-    '/trainersignup',
-    '/usersignup'
-  ].includes(location.pathname);
-
-  // 트레이너 페이지와 유저 페이지 확인
-  const trainerPages = ['/trainerprofile', '/trainermypage', '/pricelist', '/chatroom', '/chatlist'];
-  const userPages = ['/usermypage', '/trainersearch', '/userprofile','/chatroom', '/chatlist'];
-  
-  const isTrainerPage = trainerPages.includes(location.pathname);
-  const isUserPage = userPages.includes(location.pathname);
-
+  // userType이 null인 경우에 메뉴바 숨김
+  const hideMenuBar = userType === null;
 
   return (
     <header className="relative flex justify-between items-center p-4 bg-[#edf1f6] shadow-md max-w-[390px] mx-auto">
@@ -38,16 +26,17 @@ function Header() {
         <img src={logo} alt="로고" className="w-12 sm:w-16 h-auto" />
       </Link>
 
-      {/* 오른쪽 메뉴 버튼 */}
-      {!hideMenuBar && (
+      {/* 로그인 후에만 토글 버튼 보이기 */}
+      {!hideMenuBar && isLoggedIn && (
         <>
           <button onClick={toggleMenu} className="text-2xl">
             {menuOpen ? '✕' : '≡'}
           </button>
           {menuOpen && (
             <div className="absolute right-4 z-50 top-16 bg-white shadow-md rounded-lg p-4 flex flex-col space-y-2">
-              {isTrainerPage && <TrainerMenuButtons onClick={() => setMenuOpen(false)} />}
-              {isUserPage && <UserMenuButtons onClick={() => setMenuOpen(false)} />}
+              {/* 로그인한 회원의 타입에 따라 메뉴 표시 */}
+              {userType === 'trainer' && <TrainerMenuButtons onClick={() => setMenuOpen(false)} />}
+              {userType === 'user' && <UserMenuButtons onClick={() => setMenuOpen(false)} />}
             </div>
           )}
         </>
