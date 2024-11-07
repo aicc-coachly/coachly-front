da520f4e9a8be6ea209e27f32527a45a2a96a840
HHJ74 <gpgus0704@gmail.com>
2024-11-07 12:43:06 +0900

    fix : Create and render root using createRoot

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'; // Provider 추가
-import store from './redux/store'; // store 추가
+import ReactDOM from 'react-dom/client'; // createRoot를 위해 react-dom/client에서 불러오기
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 import './assets/styles/styles.css';
 
-ReactDOM.render(
+// createRoot를 사용하여 root 생성 및 렌더링
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Provider store={store}> {/* Provider로 전체 애플리케이션 감싸기 */}
+    <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
