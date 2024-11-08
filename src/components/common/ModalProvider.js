f33227f02bd4bbf558c16c5712a640d1e4109940
HHJ74 <gpgus0704@gmail.com>
2024-11-08 12:42:27 +0900

    Feat : Auto-close modal on route change for seamless navigation experience

--- src/components/common/ModalProvider.js
+++ src/components/common/ModalProvider.js
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom'; // useLocation 추가
 
 // ModalContext 및 커스텀 훅 생성
 const ModalContext = createContext();
@@ -8,6 +9,7 @@ export const useModal = () => useContext(ModalContext);
 export const ModalProvider = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [modalContent, setModalContent] = useState(null);
+  const location = useLocation(); // 현재 경로 정보 가져오기
 
   const openModal = (content) => {
     setModalContent(content);
@@ -18,6 +20,13 @@ export const ModalProvider = ({ children }) => {
     setIsOpen(false);
     setModalContent(null);
   };
+
+  // 경로가 변경될 때마다 모달을 닫음
+  useEffect(() => {
+    if (isOpen) {
+      closeModal();
+    }
+  }, [location.pathname]); // 경로가 변경될 때 실행
 
   return (
     <ModalContext.Provider value={{ isOpen, modalContent, openModal, closeModal }}>
@@ -34,7 +43,7 @@ const Modal = ({ isOpen, modalContent, closeModal }) => {
   return (
     <div className="modal-overlay fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
       <div className="w-[85%] max-w-sm bg-gray-300 p-6 rounded-lg relative">
-        <button className="absolute top-4 right-4 text-lg font-bold" onClick={closeModal} >X</button>
+        <button className="absolute top-4 right-4 text-lg font-bold" onClick={closeModal}>X</button>
         {modalContent}
       </div>
     </div>
