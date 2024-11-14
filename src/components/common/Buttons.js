c8902914f186ea2e8dc24b3aae57d6cb58804ee8
geonnim <yy4338@gmail.com>
2024-11-14 13:13:20 +0900

    style: Update signup page with dark theme and bold design

--- src/components/common/Buttons.js
+++ src/components/common/Buttons.js
@@ -1,69 +1,70 @@
 // src/components/common/Buttons.js
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { PTModal } from "../trainer/PTModal";
-import { PaymentListModal } from "../trainer/PaymentListModal";
-import { RefundPTModal } from "../user/RefundPTModal";
-import { useModal } from "../../components/common/ModalProvider";
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../../redux/slice/authSlice";
-import { persistor } from "../../redux/store";
-import { logout as authLogout } from "../../redux/slice/authSlice";
-import { logout as chatLogout } from "../../redux/slice/chatSlice";
-import { logout as paymentLogout } from "../../redux/slice/paymentSlice";
-import { logout as refundLogout } from "../../redux/slice/refundSlice";
-import { logout as scheduleLogout } from "../../redux/slice/scheduleSlice";
-import { logout as trainerLogout } from "../../redux/slice/trainerSlice";
-import { logout as userLogout } from "../../redux/slice/userSlice";
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { PTModal } from '../trainer/PTModal';
+import { PaymentListModal } from '../trainer/PaymentListModal';
+import { RefundPTModal } from '../user/RefundPTModal';
+import { useModal } from '../../components/common/ModalProvider';
+import { useDispatch } from 'react-redux';
+import { logout as authLogout } from '../../redux/slice/authSlice';
+import { persistor } from '../../redux/store';
+import { logout as chatLogout } from '../../redux/slice/chatSlice';
+import { logout as paymentLogout } from '../../redux/slice/paymentSlice';
+import { logout as refundLogout } from '../../redux/slice/refundSlice';
+import { logout as scheduleLogout } from '../../redux/slice/scheduleSlice';
+import { logout as trainerLogout } from '../../redux/slice/trainerSlice';
+import { logout as userLogout } from '../../redux/slice/userSlice';
 
-function Buttons({ size, color = "#4831D4", children, onClick }) {
+function Buttons({ size, color = '#4831D4', children, onClick }) {
   // 버튼 크기에 따라 클래스 지정
-  let buttonClasses = "rounded font-semibold flex items-center justify-center";
+  let buttonClasses = 'rounded font-semibold flex items-center justify-center';
 
   switch (size) {
-    case "big":
-      buttonClasses += " px-8 py-4 text-lg";
+    case 'big':
+      buttonClasses += ' px-8 py-4 text-lg';
       break;
-    case "middle":
-      buttonClasses += " px-6 py-3 text-base";
+    case 'middle':
+      buttonClasses += ' px-6 py-3 text-base';
       break;
-    case "small":
-      buttonClasses += " px-4 py-2 text-sm";
+    case 'small':
+      buttonClasses += ' px-4 py-2 text-sm';
       break;
     default:
-      buttonClasses += " px-6 py-3 text-base"; // 기본적으로 middle 크기로 설정
+      buttonClasses += ' px-6 py-3 text-base'; // 기본적으로 middle 크기로 설정
   }
 
   return (
     <button
       onClick={onClick}
-      className={`${buttonClasses}`}
+      className={`py-2 px-4 text-center rounded-md font-semibold tracking-wide transition duration-200 shadow-md hover:shadow-lg ${buttonClasses}`}
       style={{
         backgroundColor: color,
-        color: color === "#4831D4" ? "white" : "#CCF381",
+        color: color === '#3B82F6' ? '#3B3B3B' : 'white',
+        border: color === '#CCF381' ? '2px solid #3B3B3B' : 'none',
       }}
     >
       {children}
     </button>
   );
 }
+
 // src/components/common/Buttons.js
 
 export function UserMenuButtons({ onClick }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    dispatch(authLogout());
-    dispatch(chatLogout());
-    dispatch(paymentLogout());
-    dispatch(refundLogout());
-    dispatch(scheduleLogout());
-    dispatch(trainerLogout());
-    dispatch(userLogout());
-    persistor.purge(); // persisted 상태 초기화
-    localStorage.removeItem("persist:root"); // 추가적으로 localStorage에서 제거
-    navigate("/");
+  const handleLogout = async () => {
+    await dispatch(authLogout());
+    await dispatch(chatLogout());
+    await dispatch(paymentLogout());
+    await dispatch(refundLogout());
+    await dispatch(scheduleLogout());
+    await dispatch(trainerLogout());
+    await dispatch(userLogout());
+    await persistor.purge(); // persisted 상태 초기화
+    localStorage.removeItem('persist:root'); // 추가적으로 localStorage에서 제거
+    navigate('/');
   };
 
   return (
@@ -80,17 +81,17 @@ export function TrainerMenuButtons({ onClick }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    dispatch(authLogout());
-    dispatch(chatLogout());
-    dispatch(paymentLogout());
-    dispatch(refundLogout());
-    dispatch(scheduleLogout());
-    dispatch(trainerLogout());
-    dispatch(userLogout());
-    persistor.purge(); // persisted 상태 초기화
-    localStorage.removeItem("persist:root"); // 추가적으로 localStorage에서 제거
-    navigate("/"); // 로그인 페이지로 리디렉션
+  const handleLogout = async () => {
+    await dispatch(authLogout());
+    await dispatch(chatLogout());
+    await dispatch(paymentLogout());
+    await dispatch(refundLogout());
+    await dispatch(scheduleLogout());
+    await dispatch(trainerLogout());
+    await dispatch(userLogout());
+    await persistor.purge(); // persisted 상태 초기화
+    localStorage.removeItem('persist:root'); // 추가적으로 localStorage에서 제거
+    navigate('/'); // 로그인 페이지로 리디렉션
   };
 
   return (
@@ -104,12 +105,16 @@ export function TrainerMenuButtons({ onClick }) {
 
 export function MenuButton({ label, to, onClick }) {
   return (
-    <button
-      className="text-sm z-50 p-2 rounded bg-[#4831D4] text-white "
-      onClick={onClick}
+    <Link
+      to={to}
+      onClick={(event) => {
+        event.stopPropagation();
+        if (onClick) onClick();
+      }}
+      className="text-sm z-50 p-2 rounded bg-[#4831D4] text-white"
     >
-      <Link to={to}>{label}</Link>
-    </button>
+      {label}
+    </Link>
   );
 }
 
--- src/pages/user/UserMypage.js
+++ src/pages/user/UserMypage.js
@@ -10,36 +10,22 @@ import { getUser, getUserInbody } from '../../redux/slice/userSlice';
 import { setUser } from '../../redux/slice/authSlice';
 import { getPtschedule } from '../../redux/slice/paymentSlice';
 import { getScheduleRecord } from '../../redux/slice/scheduleSlice';
-import { createChatRoom } from "../../redux/thunks/chatThunks";
-
-// 중복 트레이너 제거 함수
-const getUniqueTrainers = (schedule) => {
-  const uniqueTrainers = [];
-  const trainerNames = new Set();
-
-  for (const item of schedule) {
-    if (!trainerNames.has(item.trainer_name)) {
-      uniqueTrainers.push(item);
-      trainerNames.add(item.trainer_name);
-    }
-  }
-
-  return uniqueTrainers;
-};
-
+import { createChatRoom } from '../../redux/thunks/chatThunks';
+import img from '../../assets/images/newlogo.png';
 
 function UserMypage() {
+  const [showCompleted, setShowCompleted] = useState(false);
   const dispatch = useDispatch();
   const { openModal } = useModal();
   const navigate = useNavigate();
   const user_number = useSelector((state) => state.auth?.user?.user_number);
-
   const inbodyData = useSelector((state) => {
     const userInbodyData = Array.isArray(state.user?.inbodyData)
       ? state.user.inbodyData
       : [];
     return userInbodyData.filter((data) => data.user_number === user_number);
   });
+
   const [scheduleRecords, setScheduleRecords] = useState([]);
   const [isFetched, setIsFetched] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
@@ -105,12 +91,49 @@ function UserMypage() {
     }
   }, [ptNumbers, isFetched]);
 
+  const handleCheckboxChange = (e) => {
+    setShowCompleted(e.target.checked);
+  };
+
   const filteredScheduleRecord = scheduleRecords.filter(
     (schedule) => schedule.status !== 'deleted'
   );
 
+  const displayedClassItems = filteredScheduleRecord.filter((schedule) =>
+    showCompleted
+      ? schedule.status === 'completed'
+      : schedule.status !== 'completed'
+  );
+  const filteredClassTotalPages = Math.ceil(
+    displayedClassItems.length / itemsPerPage
+  );
+  const paginatedClassItems = displayedClassItems.slice(
+    (classPage - 1) * itemsPerPage,
+    classPage * itemsPerPage
+  );
+
   const filteredItems = inbodyData.filter((inbody) => inbody.status);
   const totalPages = Math.ceil(filteredItems.length / itemsPerPage);
+
+  const getUniqueTrainers = (schedule) => {
+    const uniqueTrainers = [];
+    const trainerIds = new Set(); // trainer_number로 중복을 관리
+
+    for (const item of schedule) {
+      if (
+        item.trainer_number !== null &&
+        !trainerIds.has(item.trainer_number)
+      ) {
+        uniqueTrainers.push({
+          trainer_name: item.trainer_name,
+          trainer_number: item.trainer_number,
+        });
+        trainerIds.add(item.trainer_number);
+      }
+    }
+
+    return uniqueTrainers;
+  };
 
   // 중복을 제거한 트레이너 목록 생성
   const uniqueTrainers = getUniqueTrainers(pt_schedule);
@@ -121,7 +144,6 @@ function UserMypage() {
     trainerPage * itemsPerPage
   );
   const trainerTotalPages = Math.ceil(uniqueTrainers.length / itemsPerPage);
-
   const classItems = filteredScheduleRecord.slice(
     (classPage - 1) * itemsPerPage,
     classPage * itemsPerPage
@@ -160,46 +182,44 @@ function UserMypage() {
     return pageNumbers;
   };
 
-  // console.log(userId);
-  // console.log(profile);
-  // navigate("/userptschedule");
   const handleRefundPage = () => {
-    navigate("/userptschedule", { state: { pt_schedule } });
+    navigate('/userptschedule', { state: { pt_schedule } });
   };
-
   const handleMyInfoUpdate = () => {
-    navigate("/userprofile", { state: { userInfo } });
+    navigate('/userprofile', { state: { userInfo } });
   };
 
   const handleChat = async (trainer_number) => {
+    if (!user_number || !trainer_number) {
+      console.error('user_number 또는 trainer_number가 정의되지 않았습니다.', {
+        user_number,
+        trainer_number,
+      });
+      return;
+    }
+
     try {
-      if (!user_number || !trainer_number) {
-        console.log("pt_schedule:", pt_schedule);
-        console.error("user_number 또는 trainer_number가 정의되지 않았습니다.", { user_number, trainer_number });
-        return; // 값이 없으면 함수 종료
-      }
-  
-      // 채팅방 생성/확인 요청
       const response = await dispatch(
         createChatRoom({ user_number, trainer_number })
       );
-      
+
       if (response?.payload?.room_id) {
-        console.log("채팅방 생성 성공, room_id:", response.payload.room_id);
         navigate(`/chatRoom/${response.payload.room_id}`);
       } else {
-        console.warn("API 응답에서 room_id가 반환되지 않았습니다.", response.payload);
+        console.warn(
+          'API 응답에서 room_id가 반환되지 않았습니다.',
+          response.payload
+        );
       }
     } catch (error) {
-      console.error("채팅방 생성 중 오류 발생:", error);
+      console.error('채팅방 생성 중 오류 발생:', error);
     }
   };
-  
-
   return (
     <div className="max-w-[390px] mx-auto bg-gray-100 p-4">
+      {/* 내 정보 섹션 */}
       <div className="bg-white rounded-lg shadow-md p-4 mb-4 relative">
-        <h2 className="text-lg font-semibold mb-2">내 정보</h2>
+        <h2 className="text-lg font-semibold mb-2 text-gray-700">내 정보</h2>
         <button
           onClick={handleRefundPage}
           className="absolute top-4 right-[100px] px-3 py-1 bg-gray-300 text-sm rounded-full"
@@ -213,14 +233,15 @@ function UserMypage() {
           수정하기
         </button>
         <div className="flex items-start mt-4 space-x-4">
-          <div className="w-[8rem] h-[8rem] bg-gray-200 overflow-hidden"></div>
           <div className="flex-1">
             <div className="flex justify-end">
-              <p className="px-3 py-1 bg-gray-300 text-sm rounded-md">
+              <p className="px-3 py-1 bg-gray-300 text-sm rounded-md text-gray-500">
                 {profile?.user_detail_address}
               </p>
             </div>
-            <p className="mt-2 text-base font-medium">{profile?.name}</p>
+            <p className="mt-2 text-base font-medium text-gray-700">
+              {profile?.name}
+            </p>
             <p className="text-sm text-gray-500">{profile?.email}</p>
             <p className="text-sm text-gray-500">{profile?.phone}</p>
             <p className="text-sm text-gray-500">{profile?.gender}</p>
@@ -228,27 +249,32 @@ function UserMypage() {
         </div>
       </div>
 
-      <div className="bg-white rounded-lg shadow-md p-2 mb-4">
-        <h2 className="text-lg font-semibold p-2">담당 트레이너</h2>
+      {/* 담당 트레이너 섹션 */}
+      <div className="bg-white rounded-lg shadow-md p-4 mb-4">
+        <h2 className="text-lg font-semibold mb-2 text-gray-700">
+          담당 트레이너
+        </h2>
         {trainerItems.map((trainer, index) => (
           <div
             key={index}
-            className="flex items-center justify-between bg-gray-300 p-1 mb-2"
+            className="flex items-center justify-between bg-gray-100 p-2 rounded-lg mb-2 shadow-sm"
           >
-            <p className="text-base text-sm">{trainer.trainer_name}</p>
-             <button
-                  onClick={() => handleChat(trainer.trainer_number)}
-                  className="px-3 py-1 bg-pink-300 text-sm rounded-md"
-                >
-                  1:1 채팅하기
-                </button>
+            <p className="text-sm font-medium text-gray-600">
+              {trainer.trainer_name}
+            </p>
+            <button
+              onClick={() => handleChat(trainer.trainer_number)}
+              className="px-3 py-1 bg-blue-500 text-sm rounded-md text-white font-semibold"
+            >
+              1:1 채팅하기
+            </button>
           </div>
         ))}
-        <div className="flex justify-center mt-4">
+        <div className="flex justify-center mt-4 space-x-2">
           <button
             onClick={handleTrainerPrev}
             disabled={trainerPage === 1}
-            className="mx-1 px-2 py-1 bg-gray-300 rounded-md"
+            className="px-3 py-1 bg-gray-300 rounded-md text-sm"
           >
             이전
           </button>
@@ -256,11 +282,11 @@ function UserMypage() {
             <button
               key={pageNumber}
               onClick={() => handleTrainerPageChange(pageNumber)}
-              className={`mx-1 px-2 py-1 ${
+              className={`px-3 py-1 ${
                 trainerPage === pageNumber
                   ? 'bg-blue-500 text-white'
                   : 'bg-gray-300'
-              } rounded-md`}
+              } rounded-md text-sm`}
             >
               {pageNumber}
             </button>
@@ -268,22 +294,38 @@ function UserMypage() {
           <button
             onClick={handleTrainerNext}
             disabled={trainerPage === trainerTotalPages}
-            className="mx-1 px-2 py-1 bg-gray-300 rounded-md"
+            className="px-3 py-1 bg-gray-300 rounded-md text-sm"
           >
             다음
           </button>
         </div>
       </div>
 
-      <div className="bg-white rounded-lg shadow-md p-2 mb-4">
-        <h2 className="text-lg font-semibold p-2">예약된 수업</h2>
-        {classItems.length > 0 ? (
-          classItems.map((schedule) => (
+      {/* 예약된 수업 섹션 */}
+      <div className="bg-white rounded-lg shadow-md p-4 mb-4">
+        <h2 className="text-lg font-semibold mb-2 text-gray-700">
+          예약된 수업
+        </h2>
+        <div className="flex items-center mb-4">
+          <label className="text-sm mr-2 text-gray-600">
+            완료된 수업만 보기
+          </label>
+          <input
+            type="checkbox"
+            checked={showCompleted}
+            onChange={handleCheckboxChange}
+            className="form-checkbox"
+          />
+        </div>
+        {paginatedClassItems.length > 0 ? (
+          paginatedClassItems.map((schedule) => (
             <div
               key={schedule.schedule_number}
-              className="flex items-center justify-between bg-gray-300 p-1 mb-2"
+              className="flex items-center justify-between bg-gray-100 p-2 rounded-lg mb-2 shadow-sm"
             >
-              <p className="text-base text-sm">{schedule.trainer_name}</p>
+              <p className="text-sm font-medium text-gray-600">
+                {schedule.trainer_name}
+              </p>
               <span className="text-sm text-gray-500">
                 {new Date(schedule.class_date).toLocaleDateString()}
                 {schedule.status === 'completed' && (
@@ -294,52 +336,57 @@ function UserMypage() {
                 onClick={() =>
                   openModal(<CheckScheduleModal schedule={schedule} />)
                 }
-                className="text-center px-3 py-1 bg-pink-300 text-sm rounded-md"
+                className="px-3 py-1 bg-blue-500 text-sm rounded-md text-white font-semibold"
               >
                 자세히 보기
               </button>
             </div>
           ))
         ) : (
-          <p>예약된 수업이 없습니다.</p>
+          <p className="text-center text-gray-500">
+            해당 조건의 수업이 없습니다.
+          </p>
         )}
-        <div className="flex justify-center mt-4">
+        <div className="flex justify-center mt-4 space-x-2">
           <button
             onClick={handleClassPrev}
             disabled={classPage === 1}
-            className="mx-1 px-2 py-1 bg-gray-300 rounded-md"
+            className="px-3 py-1 bg-gray-300 rounded-md text-sm"
           >
             이전
           </button>
-          {getPageNumbers(classTotalPages).map((pageNumber) => (
+          {getPageNumbers(filteredClassTotalPages).map((pageNumber) => (
             <button
               key={pageNumber}
               onClick={() => handleClassPageChange(pageNumber)}
-              className={`mx-1 px-2 py-1 ${
+              className={`px-3 py-1 ${
                 classPage === pageNumber
                   ? 'bg-blue-500 text-white'
                   : 'bg-gray-300'
-              } rounded-md`}
+              } rounded-md text-sm`}
             >
               {pageNumber}
             </button>
           ))}
           <button
             onClick={handleClassNext}
-            disabled={classPage === classTotalPages}
-            className="mx-1 px-2 py-1 bg-gray-300 rounded-md"
+            disabled={classPage === filteredClassTotalPages}
+            className="px-3 py-1 bg-gray-300 rounded-md text-sm"
           >
             다음
           </button>
         </div>
       </div>
 
-      <div className="bg-white rounded-lg shadow-md p-2 mb-4">
-        <div className="flex justify-between p-2">
-          <h2 className="text-lg font-semibold">나의 체성분 기록</h2>
+      {/* 체성분 기록 섹션 */}
+      <div className="bg-white rounded-lg shadow-md p-4 mb-4">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-lg font-semibold text-gray-700">
+            나의 체성분 기록
+          </h2>
           <button
             onClick={() => openModal(<BodyCompositionModal />)}
-            className="text-center px-3 py-1 bg-pink-300 text-sm rounded-md"
+            className="px-3 py-1 bg-blue-500 text-sm rounded-md text-white font-semibold"
           >
             추가하기
           </button>
@@ -350,27 +397,27 @@ function UserMypage() {
           .map((inbody, index) => (
             <div
               key={index}
-              className="flex items-center justify-between bg-gray-300 p-1 mb-2"
+              className="flex items-center justify-between bg-gray-100 p-2 rounded-lg mb-2 shadow-sm"
             >
-              <p className="text-base text-sm">
+              <p className="text-sm font-medium text-gray-600">
                 측정날짜: {formatDate(inbody.user_measurement_date)}
               </p>
               <button
                 onClick={() =>
                   openModal(<EditBodyCompositionModal inbodyData={inbody} />)
                 }
-                className="text-center px-3 py-1 bg-pink-300 text-sm rounded-md"
+                className="px-3 py-1 bg-blue-500 text-sm rounded-md text-white font-semibold"
               >
                 더보기
               </button>
             </div>
           ))}
 
-        <div className="flex justify-center mt-4">
+        <div className="flex justify-center mt-4 space-x-2">
           <button
             onClick={handlePrev}
             disabled={currentPage === 1}
-            className="mx-1 px-2 py-1 bg-gray-300 rounded-md"
+            className="px-3 py-1 bg-gray-300 rounded-md text-sm"
           >
             이전
           </button>
@@ -378,11 +425,11 @@ function UserMypage() {
             <button
               key={pageNumber}
               onClick={() => handlePageChange(pageNumber)}
-              className={`mx-1 px-2 py-1 ${
+              className={`px-3 py-1 ${
                 currentPage === pageNumber
                   ? 'bg-blue-500 text-white'
                   : 'bg-gray-300'
-              } rounded-md`}
+              } rounded-md text-sm`}
             >
               {pageNumber}
             </button>
@@ -390,7 +437,7 @@ function UserMypage() {
           <button
             onClick={handleNext}
             disabled={currentPage === totalPages}
-            className="mx-1 px-2 py-1 bg-gray-300 rounded-md"
+            className="px-3 py-1 bg-gray-300 rounded-md text-sm"
           >
             다음
           </button>
